0c887767fabf3f21aa054c77925a6bad4ef4c913
Carth501 <carth501@gmail.com>
2024-04-19 13:07:14 -0400

    apply fix

--- scripts/display/SupplyDisplay.gd
+++ scripts/display/SupplyDisplay.gd
@@ -5,13 +5,13 @@ class_name Supply_Display extends Control
 @export var max_label : RichTextLabel
 @export var delta_label : RichTextLabel
 
-func set_id(id : String, supply_collection : Supply_Collection):
+func setup(id : String, supply_collection : Supply_Collection):
 	set_name_display(id)
 	var supply : Supply = supply_collection.get_supply(id)
 	set_value_display(supply.value)
 	supply.update_value.connect(set_value_display)
-	set_max_display(supply.value)
-	supply.update_max.connect(set_value_display)
+	set_max_display(supply.v_max)
+	supply.update_max.connect(set_max_display)
 	print(id)
 
 func set_name_display(id : String):
