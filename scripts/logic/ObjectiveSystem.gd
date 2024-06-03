ac9bab0b0f8f69013ae5a984b3b571808e4db74c
Carth501 <carth501@gmail.com>
2024-06-03 19:32:33 -0400

    implement objective checking

--- scripts/logic/ObjectiveSystem.gd
+++ scripts/logic/ObjectiveSystem.gd
@@ -12,6 +12,7 @@ func _ready():
 	next()
 
 func next():
+	unsubscribe_supplies()
 	var new_objective = future_objectives.pop_front()
 	if(new_objective == null):
 		print("VICTORY")
@@ -23,3 +24,54 @@ func next():
 			await supply_collection.new_supply
 			supply = supply_collection.get_supply(supply_id)
 		supply.set_objective(active_objective[supply_id])
+		supply.update_value.connect(check_victory)
+		check_victory(0)
+
+func check_victory(_value):
+	if(check_conditions()):
+		next()
+
+func check_conditions():
+	for supply_id in active_objective:
+		var supply = supply_collection.get_supply(supply_id)
+		var criteria_list = active_objective[supply_id]
+		for criteria in criteria_list:
+			if(criteria.has("less_than")):
+				return check_less_than(supply, criteria.less_than)
+			elif(criteria.has("equal_to")):
+				return check_equal_to(supply, criteria.equal_to)
+			elif(criteria.has("greater_than")):
+				return check_greater_than(supply, criteria.greater_than)
+
+func unsubscribe_supplies():
+	for supply_id in active_objective:
+		var supply = supply_collection.get_supply(supply_id)
+		supply.update_value.disconnect(check_victory)
+		supply.unset_objective()
+
+func check_less_than(supply : Supply, criteria : Dictionary) -> bool:
+	if(criteria.has("supply")):
+		var id = criteria["supply"]
+		var other = supply_collection.get_supply(id)
+		return supply.value < other.value
+	else:
+		var value = criteria["const"]
+		return supply.value < value
+
+func check_equal_to(supply : Supply, criteria : Dictionary) -> bool:
+	if(criteria.has("supply")):
+		var id = criteria["supply"]
+		var other = supply_collection.get_supply(id)
+		return supply.value == other.value
+	else:
+		var value = criteria["const"]
+		return supply.value == value
+
+func check_greater_than(supply : Supply, criteria : Dictionary) -> bool:
+	if(criteria.has("supply")):
+		var id = criteria["supply"]
+		var other = supply_collection.get_supply(id)
+		return supply.value > other.value
+	else:
+		var value = criteria["const"]
+		return supply.value > value
