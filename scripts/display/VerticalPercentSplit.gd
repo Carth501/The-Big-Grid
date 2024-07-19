9dc9737cb4d290997987de7aed888c2b4bf87727
Carth501 <carth501@gmail.com>
2024-07-19 16:57:53 -0400

    implement percent split for both splits

---
+++ scripts/display/VerticalPercentSplit.gd
@@ -0,0 +1,9 @@
+extends VSplitContainer
+
+@export var split := 0.6
+
+func _on_dragged(offset: int) -> void:
+	split = offset / size.y
+
+func _on_resized() -> void:
+	split_offset = size.y * split
