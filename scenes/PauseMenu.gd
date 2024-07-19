cc850a98aca3d030b776747c5c6be8a3dda26234
Krampy <162400557+KrampyKranky@users.noreply.github.com>
2024-07-19 01:17:20 -0400

    Added pause menu
    Added pause menu with Pause, Resume and Quit functions. Integrating Save panel may need a refactor, and unsure if load button should be included or kept in main menu.

---
+++ scenes/PauseMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+#Make sure root node process is set to always or will not work properly
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("pause"): #Takes input  for pause key
+		pause_or_unpause() #executes pause/unpause function
+	
+	
+func pause_or_unpause():
+		if get_tree().paused == true: #if game is paused, pressing resume will unpause it
+			$".".hide()
+			get_tree().paused = false
+		elif get_tree().paused == false: #if game is unpaused, pressing escape will pause it
+			$".".show()
+			get_tree().paused = true
+func _quitGame(): #Quits to main menu
+	get_tree().paused = false #unpauses tree so main menu can be clicked
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn") #changes scene to main menu
