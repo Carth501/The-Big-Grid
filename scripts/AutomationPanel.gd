c7562900154ad0192febc97024b2bdb57b12d383
Carth501 <carth501@gmail.com>
2024-07-24 18:27:53 -0400

    Implement machine organization system

--- scripts/AutomationPanel.gd
+++ scripts/AutomationPanel.gd
@@ -1,15 +1,20 @@
 extends ColorRect
 
 @onready var machine_editor_prefab := preload("res://scenes/display/ActionMachineEditor.tscn")
-@export var machine_container : HFlowContainer
+@onready var action_section_prefab:= preload("res://scenes/display/ActionSection.tscn")
+@export var action_section_container : Control
 @export var machine_factory : Machine_Factory
-
-func _ready() -> void:
-	var machine_registry = machine_factory.machine_registry
-	for machine_id in machine_registry:
-		add_machine_editor(machine_registry[machine_id])
+@export var machine_editors : Dictionary
+@export var action_sections : Dictionary
 
 func add_machine_editor(new_machine : Machine):
+	var action_id = new_machine.action.id
+	if(!action_sections.has(action_id)):
+		var new_action_section = action_section_prefab.instantiate()
+		action_section_container.add_child(new_action_section)
+		var action_name = ActionTranslatorSingle.data[action_id].name
+		new_action_section.set_label(action_name)
+		action_sections[action_id] = new_action_section
 	var new_machine_editor = machine_editor_prefab.instantiate()
 	new_machine_editor.set_machine(new_machine)
-	machine_container.add_child(new_machine_editor)
+	action_sections[action_id].add_machine_editor(new_machine_editor)
