8ac4dd2f0e674abc4f03b0d0a930120acc61b984
Krampy <162400557+KrampyKranky@users.noreply.github.com>
2024-07-25 22:34:21 -0400

    Implemented effects slider
    Implemented effects slider to options menu

--- scripts/singletons/SettingsDataContainer.gd
+++ scripts/singletons/SettingsDataContainer.gd
@@ -4,12 +4,14 @@ signal windowed_mode_change(index : int)
 signal resolution_change(index : int)
 signal master_volume_change(level : float)
 signal music_volume_change(level : float)
+signal effects_volume_change(level : float)
 signal keep_position_change(float)
 
 var window_mode_index: int = 0
 var resolution_index : int = 0
 var master_volume : float = 0.0
 var music_volume : float = 0.0
+var effects_volume: float = 0.0
 var keep_positions : bool = true
 var focus_filtering : bool = false
 
@@ -22,7 +24,8 @@ var default_settings_data := {
 		"window_mode_index" : 0,
 		"resolution_index" : 0,
 		"master_volume" : 50,
-		"music_volume" : 50
+		"music_volume" : 50,
+		"effects_volume" : 50
 	}
 
 func _ready():
@@ -40,6 +43,9 @@ func get_master_volume() -> float:
 func get_music_volume() -> float:
 	return music_volume
 
+func get_effects_volume() -> float:
+	return effects_volume
+	
 func set_keep_position(new_setting : bool):
 	keep_positions = new_setting
 	on_settings_save()
@@ -51,6 +57,7 @@ func on_settings_save() -> void:
 	config.set_value("Video", "resolution_index", resolution_index)
 	config.set_value("Audio", "master_volume", master_volume)
 	config.set_value("Audio", "music_volume", music_volume)
+	config.set_value("Audio", "effects_volume", effects_volume)
 	config.set_value("Customization", "keep_positions", keep_positions)
 	config.set_value("Customization", "focus_filtering", focus_filtering)
 	
@@ -67,11 +74,13 @@ func load_settings_data() -> void:
 		resolution_index = default_settings_data["resolution_index"]
 		master_volume = default_settings_data["master_volume"]
 		music_volume = default_settings_data["music_volume"]
+		effects_volume = default_settings_data["effects_volume"]
 	else:
 		window_mode_index = config.get_value("Video", "window_mode_index", 0)
 		resolution_index = config.get_value("Video", "resolution_index", 0)
 		master_volume = config.get_value("Audio", "master_volume", 50)
 		music_volume = config.get_value("Audio", "music_volume", 50)
+		effects_volume = config.get_value("Audio", "effects_volume", 50)
 		keep_positions = config.get_value("Customization", "keep_positions", true)
 		focus_filtering = config.get_value("Customization", "focus_filtering", false)
 
