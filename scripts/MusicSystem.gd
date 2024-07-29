fd000cd1b12358aed90c949a80d9f3410fbf148a
Carth501 <carth501@gmail.com>
2024-07-29 17:45:49 -0400

    Implement victory music

--- scripts/MusicSystem.gd
+++ scripts/MusicSystem.gd
@@ -1,7 +1,9 @@
-extends Node
+class_name MusicSystem extends Node
 
 @export var music_players: Array[AudioStreamPlayer]
+@export var victory_music: AudioStreamPlayer
 var index = 0;
+var fade_duration = 2;
 
 func _ready() -> void:
 	var master_volume = SettingsDataContainerSingle.master_volume
@@ -11,7 +13,6 @@ func _ready() -> void:
 	if(music_players.size() > 0):
 		index = randi_range(0, music_players.size() - 1)
 	music_players[index].play()
-		
 
 func next_track():
 	var new_index = randi_range(0, music_players.size() - 1)
@@ -19,3 +20,14 @@ func next_track():
 		new_index = randi_range(0, music_players.size() - 1)
 	index = new_index
 	music_players[index].play()
+
+func play_victory():
+	var tween = create_tween()
+	tween.tween_property(music_players[index], "volume_db", -44.768, fade_duration)
+	await get_tree().create_timer(fade_duration).timeout
+	music_players[index].set_stream_paused(true)
+	victory_music.play()
+	await victory_music.finished
+	music_players[index].set_stream_paused(false)
+	tween.tween_property(music_players[index], "volume_db", 44.768, fade_duration)
+	
