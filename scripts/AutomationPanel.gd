c69f6ca1f536c333856eabbc9f1d60654c4b63e1
Carth501 <carth501@gmail.com>
2024-07-30 15:24:42 -0400

    improve resizing behavior

--- scripts/AutomationPanel.gd
+++ scripts/AutomationPanel.gd
@@ -1,5 +1,6 @@
 extends ColorRect
 
+signal recalculate_all
 @onready var machine_editor_prefab := preload("res://scenes/display/ActionMachineEditor.tscn")
 @onready var action_section_prefab:= preload("res://scenes/display/ActionSection.tscn")
 @export var action_section_container : Control
@@ -9,12 +10,20 @@ extends ColorRect
 
 func add_machine_editor(new_machine : Machine):
 	var action_id = new_machine.action.id
+	var new_machine_editor = machine_editor_prefab.instantiate()
+	new_machine_editor.set_machine(new_machine)
 	if(!action_sections.has(action_id)):
 		var new_action_section = action_section_prefab.instantiate()
 		action_section_container.add_child(new_action_section)
 		var action_name = ActionTranslatorSingle.data[action_id].name
 		new_action_section.set_label(action_name)
 		action_sections[action_id] = new_action_section
-	var new_machine_editor = machine_editor_prefab.instantiate()
-	new_machine_editor.set_machine(new_machine)
+		recalculate_all.connect(new_action_section.recalculate_minimum_size)
 	action_sections[action_id].add_machine_editor(new_machine_editor)
+	recalculate_all.emit()
+
+func _on_visibility_changed() -> void:
+	recalculate_all.emit()
+
+func _on_scroll_container_resized() -> void:
+	recalculate_all.emit()
