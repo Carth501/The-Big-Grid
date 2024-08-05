17f4a32ce388efbaeb607be7eca846235943c078
Carth501 <carth501@gmail.com>
2024-08-05 15:44:48 -0400

    add expansion functionality to machines panel

--- scripts/display/ActionSection.gd
+++ scripts/display/ActionSection.gd
@@ -1,7 +1,8 @@
 class_name Action_Section extends Control
 
-@export var section_label : Label
+@export var section_label : Button
 @export var editor_container : HFlowContainer
+var expanded := false
 
 func set_label(section_name : String):
 	section_label.text = section_name
@@ -10,8 +11,19 @@ func add_machine_editor(new_editor : Action_Machine_Editor):
 	editor_container.add_child(new_editor)
 
 func recalculate_minimum_size():
-	var vertical = editor_container.size.y + section_label.size.y
-	custom_minimum_size.y = vertical
+	if(expanded):
+		var vertical = editor_container.size.y + section_label.size.y + 8
+		custom_minimum_size.y = vertical
+	else:
+		custom_minimum_size.y = section_label.size.y
 
 func _on_h_flow_container_child_entered_tree(_node: Node) -> void:
+	recalculate_minimum_size()
+
+func toggle_expand():
+	expand(!expanded)
+
+func expand(setting : bool):
+	expanded = setting
+	editor_container.visible = setting
 	recalculate_minimum_size()
