2777d68143a0fe0115499b550521032d38c4c00d
Carth501 <carth501@gmail.com>
2024-09-11 14:37:02 -0400

    implement development button hiding

--- scripts/display/DevelopmentDisplay.gd
+++ scripts/display/DevelopmentDisplay.gd
@@ -16,7 +16,29 @@ func _ready():
 		var dev_logic = development_handler.full_development_catalog[option]
 		new_dev_button.connect_to_logic(dev_logic)
 		new_dev_button.attempt.connect(attempt_development)
+		new_dev_button.deindex.connect(deindex)
 
 func attempt_development(id : String):
 	development_handler.attempt_development(id)
 
+func show_all_developments():
+	for dev_button_id in dev_buttons:
+		var dev_button = dev_buttons[dev_button_id]
+		if(dev_button.revealed):
+			dev_button.visible = true
+
+func get_hidden_list() -> Array:
+	var hidden_developments := []
+	for dev_button_id in dev_buttons:
+		var dev_button = dev_buttons[dev_button_id]
+		if(!dev_button.visible && dev_button.revealed):
+			hidden_developments.append(dev_button_id)
+	return hidden_developments
+
+func load_hidden_list(list : Array):
+	for id in list:
+		if(dev_buttons.has(id)):
+			dev_buttons[id].visible = false
+
+func deindex(id : String):
+	dev_buttons.erase(id)
