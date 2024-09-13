dcbd2e5cecdbb3be5afe1f229809ce5f5463baca
Carth501 <carth501@gmail.com>
2024-09-12 22:33:52 -0400

    Improve objective processing

--- scripts/logic/ObjectiveSystem.gd
+++ scripts/logic/ObjectiveSystem.gd
@@ -22,36 +22,33 @@ func next():
 			music_system.play_victory()
 		return
 	active_objective = new_objective
-	for supply_id in active_objective:
-		if(supply_id == "stepId"):
-			var stepId = active_objective[supply_id]
-			newStepDescription.emit(ObjectivesTextsSingle.data[stepId])
-		else:
-			var supply = supply_collection.get_or_create_supply(supply_id)
-			while(supply == null):
-				await supply_collection.new_supply
-				supply = supply_collection.get_supply(supply_id)
-			supply.set_objective(active_objective[supply_id])
-			supply.update_value.connect(check_victory)
-			check_victory(0)
-			supply.reveal()
+	var stepId = active_objective["stepId"]
+	newStepDescription.emit(ObjectivesTextsSingle.data[stepId])
+	for supply_id in active_objective["supplies"]:
+		var supply = supply_collection.get_or_create_supply(supply_id)
+		while(supply == null):
+			await supply_collection.new_supply
+			supply = supply_collection.get_supply(supply_id)
+		supply.set_objective(active_objective["supplies"][supply_id])
+		supply.update_value.connect(check_victory)
+		check_victory(0)
+		supply.reveal()
 
 func check_victory(_value):
 	if(check_conditions()):
 		next()
 
 func check_conditions():
-	for supply_id in active_objective:
-		if(supply_id != "stepId"):
-			var supply = supply_collection.get_supply(supply_id)
-			var criteria_list = active_objective[supply_id]
-			for criteria in criteria_list:
-				if(criteria.has("less_than")):
-					return check_less_than(supply, criteria.less_than)
-				elif(criteria.has("equal_to")):
-					return check_equal_to(supply, criteria.equal_to)
-				elif(criteria.has("greater_than")):
-					return check_greater_than(supply, criteria.greater_than)
+	var criteria_list = active_objective["supplies"]
+	for supply_id in criteria_list:
+		var supply = supply_collection.get_supply(supply_id)
+		var criteria = criteria_list[supply_id]
+		if(criteria.has("less_than")):
+			return check_less_than(supply, criteria.less_than)
+		elif(criteria.has("equal_to")):
+			return check_equal_to(supply, criteria.equal_to)
+		elif(criteria.has("greater_than")):
+			return check_greater_than(supply, criteria.greater_than)
 
 func unsubscribe_supplies():
 	for supply_id in active_objective:
